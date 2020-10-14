8edf0cc33bc68024ffc562b02a1a55f10ba413f7
Ihor iOS <igorcernis@gmail.com>
2020-10-14 13:48:46 +0300

    initial commit

---
+++ Code Sample 1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Code Sample 1.0
+//
+//  Created by Игорь Черныш on 14.10.2020.
+//  Copyright © 2020 Игорь Черныш. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
